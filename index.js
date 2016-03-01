79b4f0e8f2ffdcff17fa5cbb176435b54736827f
Lisa <lisaleung2291@gmail.com>
2016-03-01 00:34:04 -0800

    fix header attempt

--- index.js
+++ index.js
@@ -24,8 +24,19 @@ var app = express();
         var dateObj = { "unix": unix, "natural": natural };
         res.send(JSON.stringify(dateObj));
         
+        function natToUnix(date) {
+        // Conver from natural date to unix timestamp
+        return moment(date, "MMMM D, YYYY").format("X");
+    }
+    
+    function unixToNat(unix) {
+        // Convert unix timestamp to natural date
+        return moment.unix(unix).format("MMMM D, YYYY");
+    }
+    
     });
     
+    /*
     function natToUnix(date) {
         // Conver from natural date to unix timestamp
         return moment(date, "MMMM D, YYYY").format("X");
@@ -37,7 +48,7 @@ var app = express();
     }
     
     
-
+	*/
 
 
 
