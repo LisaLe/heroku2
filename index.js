3f13edcb00bf2078bfe13b409cb25d1afc0c8d93
Lisa <lisaleung2291@gmail.com>
2016-03-01 00:36:38 -0800

    fix header attempt

--- index.js
+++ index.js
@@ -1,66 +1,10 @@
-  
+
 var express = require('express');
 var moment = require('moment');
 var app = express();
 
 
-    app.get('/:query', function(req, res) {
-        var date = req.params.query;
-        var unix = null;
-        var natural = null;
-        
-        // Check for initial unix time
-        if (+date >= 0) {
-            unix = +date;
-            natural = unixToNat(unix);
-        } 
-        
-        // Check for initial natural time
-        if (isNaN(+date) && moment(date, "MMMM D, YYYY").isValid()) {
-            unix = natToUnix(date);
-            natural = unixToNat(unix);
-        }
-        
-        var dateObj = { "unix": unix, "natural": natural };
-        res.send(JSON.stringify(dateObj));
-        
-        function natToUnix(date) {
-        // Conver from natural date to unix timestamp
-        return moment(date, "MMMM D, YYYY").format("X");
-    }
-    
-    function unixToNat(unix) {
-        // Convert unix timestamp to natural date
-        return moment.unix(unix).format("MMMM D, YYYY");
-    }
-    
-    });
-    
-    /*
-    function natToUnix(date) {
-        // Conver from natural date to unix timestamp
-        return moment(date, "MMMM D, YYYY").format("X");
-    }
-    
-    function unixToNat(unix) {
-        // Convert unix timestamp to natural date
-        return moment.unix(unix).format("MMMM D, YYYY");
-    }
-    
-    
-	*/
-
-
-
-
-
-/*
-var express = require('express');
-var moment = require('moment');
-var app = express();
-
-
-app.set('port', (process.env.PORT || 5000));
+//app.set('port', (process.env.PORT || 5000));
 
 
 
@@ -87,7 +31,7 @@ app.get('/:query', function(req, res) {
     }
   
 });
-*/
+
 /*
 
 var server = app.listen(8081, function () {
